d85abec76269b5adfbf2e60a37c63ecc7a664c8c
Frédéric CELERSE <45543646+Fredericcelerse@users.noreply.github.com>
2025-12-14 17:12:26 +0100

    Create config.js

---
+++ public/config.js
@@ -0,0 +1,11 @@
+// Renseigne tes infos Supabase
+window.CONFIG = {
+  supabase_url: "https://wwosfmqzgwxpgxfnqpmu.supabase.co",
+  supabase_anon_key: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Ind3b3NmbXF6Z3d4cGd4Zm5xcG11Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjU3MTY1NzIsImV4cCI6MjA4MTI5MjU3Mn0.YlyM4vBmxhlREHrYkgS5ep0CNU1zHtCMQjJIPrCtEA8",
+
+  // URL de tes Edge Functions
+  fn_creer_standard: "https://wwosfmqzgwxpgxfnqpmu.co/functions/v1/creer_demande_standard",
+  fn_creer_speciale: "https://wwosfmqzgwxpgxfnqpmu.co/functions/v1/creer_demande_speciale",
+  fn_modifier_client: "https://wwosfmqzgwxpgxfnqpmu.supabase.co/functions/v1/modifier_demande_client",
+  fn_accepter_moniteur: "https://wwosfmqzgwxpgxfnqpmu.supabase.co/functions/v1/moniteur_accepter_initiation",
+};
